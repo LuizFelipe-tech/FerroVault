a9dd94937563c87f09994c3d7972b345c4813ed9
Luiz Felipe <luizfelipemelos@gmail.com>
2026-03-22 10:11:19 -0300

    feat(main): implement initial mock cli logic
    Replaces the initial boilerplate math demo with a representative CLI
    interaction flow, providing a foundation for upcoming cryptographic
    feature implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-// Copyright (c) 2026 Luiz Felipe. All rights reserved.
-// Licensed under the Apache License, Version 2.0.
-// See the LICENSE file in the project root for full license information.
+// Copyright (c) 2026-Present Luiz Felipe do Nascimento Melos. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// SPDX-License-Identifier: Apache-2.0
 
 //! # FerroVault CLI Entrypoint
 //!
@@ -11,6 +14,7 @@
 //! - **Logic Decoupling:** Must not implement cryptographic or system-level logic; it acts strictly as a proxy to `lib.rs`.
 //! - **Process Integrity:** Responsible for mapping internal library errors to POSIX-compliant exit codes and user-friendly terminal feedback.
 
+use std::string::String;
 use clap::Parser;
 
 mod laboratorio {
@@ -20,15 +24,22 @@ mod laboratorio {
 }
 #[derive(Parser)]
 struct Cli {
-    main_num: i32,
-    secondary_num: i32,
+    text: String,
 }
-fn somar(a: i32, b: i32) -> i32 {
-    a + b
+
+
+fn mock_encrypt(text: &str) -> String {
+    let reverse_text = text.chars().rev().collect::<String>();
+    reverse_text
 }
+
 fn main() {
     let args = Cli::parse();
+
     println!("A forja do FerroVault está acesa. 🦀");
     laboratorio::abrir_portal();
-    println!("A soma entre {args.main_num} e {args.secondary_menu}");
+    let typed_text = args.text;
+    let rev_text: String = mock_encrypt(&typed_text);
+    println!("Texto digitado: {}", typed_text);
+    println!("Texto invertido: {}", rev_text)
 }
